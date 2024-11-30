58eb042a8c087095a8dd961cce87297e30f681bb
samukaii <samuelalejandro.dev@gmail.com>
2024-11-29 21:30:33 -0300

    feature: Load database credentials from .env file
    Introduce dotenv configuration to manage database connection details. Environment variables DB_URL, DB_USERNAME, and DB_PASSWORD are now loaded from a .env file to improve security and flexibility. This change centralizes credential management and reduces hard-coded values in the source code.

--- src/main/java/org/samuel/storemanagement/StoreManagementApplication.java
+++ src/main/java/org/samuel/storemanagement/StoreManagementApplication.java
@@ -1,5 +1,6 @@
 package org.samuel.storemanagement;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreManagementApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		String dbUrl = dotenv.get("DB_URL");
+		System.setProperty("DB_URL", dbUrl);
+
+		String dbUsername = dotenv.get("DB_USERNAME");
+		System.setProperty("DB_USERNAME", dbUsername);
+
+		String dbPassword = dotenv.get("DB_PASSWORD");
+		System.setProperty("DB_PASSWORD", dbPassword);
+
 		SpringApplication.run(StoreManagementApplication.class, args);
 	}
 
