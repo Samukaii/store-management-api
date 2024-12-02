38f98d14394e8d6818ac757d05f999dcd2b35865
samukaii <samuelalejandro.dev@gmail.com>
2024-12-02 14:35:40 -0300

    feature: ensure container restarts unless stopped
    Add the --restart unless-stopped option to the docker run command. This ensures that the container restarts automatically unless it is explicitly stopped, improving the service's reliability.

--- docker-run.sh
+++ docker-run.sh
@@ -26,6 +26,6 @@ echo "Imagem '$image_name' criada com sucesso."
 
 # Rodar um novo container
 echo "Iniciando um novo container '$container_name'..."
-docker run -d --name "$container_name" --env-file .env -p 7808:8080 "$image_name" > /dev/null 2>&1
+docker run -d --name "$container_name" --restart unless-stopped --env-file .env -p 7808:8080 "$image_name" > /dev/null 2>&1
 echo "Projeto web iniciado na url http://localhost:4000 ."
 
