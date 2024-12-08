8627baede45cb1eb63ff4fad0377c56d685134ca
samukaii <samuelalejandro.dev@gmail.com>
2024-12-08 13:57:56 -0300

    fix(#unknown): Update total cost of ingredients
    - Added `PreparationChangeEvent` listener to
      `ProductIngredientEventHandler` to update the total cost
      of ingredients when a preparation is changed.
    - Added a null check for ingredients in
      `PreparationChangeEvent` to avoid
      NullPointerException.
    - Iterates over product ingredients and recalculates and
      saves them.

--- src/main/java/org/samuel/storemanagement/domain/product/ingredient/events/ProductIngredientEventHandler.java
+++ src/main/java/org/samuel/storemanagement/domain/product/ingredient/events/ProductIngredientEventHandler.java
@@ -1,6 +1,7 @@
 package org.samuel.storemanagement.domain.product.ingredient.events;
 
 import lombok.RequiredArgsConstructor;
+import org.samuel.storemanagement.domain.preparation.preparation.events.PreparationChangeEvent;
 import org.samuel.storemanagement.domain.product.ingredient.services.ProductIngredientService;
 import org.samuel.storemanagement.domain.rawMaterial.rawMaterial.events.RawMaterialPersistEvent;
 import org.springframework.context.event.EventListener;
@@ -18,6 +19,14 @@ public class ProductIngredientEventHandler {
         if(event.getRawMaterial().getIngredients() == null) return;
 
         event.getRawMaterial().getIngredients().forEach(service::recalculateAndSave);
+    }
+
+    @EventListener
+    @Async
+    public void updateTotalCost(PreparationChangeEvent event) {
+        if(event.getPreparation().getIngredients() == null) return;
+
+        event.getPreparation().getProductIngredients().forEach(service::recalculateAndSave);
     }
 }
 
